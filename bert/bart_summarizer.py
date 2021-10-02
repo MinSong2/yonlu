d6b588430d669bca5ba7737dcce2ce0297d808ec
Min Song <min.song@yonsei.ac.kr>
2021-10-02 23:13:54 +0900

    Add files via upload

--- bert/bart_summarizer.py
+++ bert/bart_summarizer.py
@@ -45,7 +45,23 @@ class BartSumSummarizer():
                     bart = BartForConditionalGeneration.from_pretrained(pretrained)
                     tokenizer = BartTokenizer.from_pretrained(pretrained)
                 elif mode == 'ko':
-                    bart = BartForConditionalGeneration.from_pretrained('hyunwoongko/kobart')
+                    if os.path.exists('kobart_summary/config.json') and os.path.exists(
+                            'kobart_summary/pytorch_model.bin'):
+                        print("== Data existed.==")
+                        pass
+                    else:
+                        os.mkdir("./kobart_summary")
+                        url = "https://drive.google.com/uc?id=1H13loH6dS_2c2Z21kaBtgz42QsjkdAwO"
+                        output = './kobart_summary/config.json'
+                        print("Download config.json")
+                        gdown.download(url, output, quiet=False)
+
+                        url = "https://drive.google.com/uc?id=1D7BAXK_0faWW39c0ptE3FtROsVRbTNwI"
+                        output = './kobart_summary/pytorch_model.bin'
+                        print("Download pytorch_model.bin")
+                        gdown.download(url, output, quiet=False)
+
+                    bart = BartForConditionalGeneration.from_pretrained('./kobart_summary')
                     tokenizer = PreTrainedTokenizerFast.from_pretrained('hyunwoongko/kobart')
                 self.tokenizer = tokenizer
             else:
