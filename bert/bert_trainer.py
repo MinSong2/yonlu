f8b2279fa383fdd083f1f5d04cc5ef240cc9d05f
Min Song <min.song@yonsei.ac.kr>
2021-10-06 09:06:17 +0900

    Add files via upload

--- bert/bert_trainer.py
+++ bert/bert_trainer.py
@@ -105,7 +105,7 @@ class PYBERTTrainer:
                 scheduler.step()
                 optimizer.zero_grad()
 
-        avg_train_avg = float(correct_predictions) / float(len(data_loader))*10
+        avg_train_avg = float(correct_predictions) / float(len(data_loader.dataset))*100
         # Calculate the average loss over all of the batches.
         avg_train_loss = total_train_loss / len(data_loader)
 
