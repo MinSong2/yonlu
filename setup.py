8b4101cac8884bae6aaf7c6feaf2c680a2178928
Min Song <min.song@yonsei.ac.kr>
2021-10-22 22:49:39 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 import setuptools
+import os
+from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+name="yonlu"
+
 #torch for CPU only
 #pip install torch==1.5.1+cpu torchvision==0.6.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
+# Build a list of all project modules
+packages = find_packages(exclude = [])
+
+print('packages', packages)
+
 setuptools.setup(
     name="yonlu", # Replace with your own username
-    version="1.1.4",
+    version="1.1.12",
     author="Min Song",
     author_email="min.song@yonsei.ac.kr",
     description="A deep learning based natural language understanding module",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MinSong2/yonlu",
-    packages=setuptools.find_packages(exclude = []),
+    packages=packages,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -55,7 +64,8 @@ setuptools.setup(
         "benepar>=0.2.0",
         "boto3",
         "kobert-transformers",
-        "treform"
+        "treform",
+        "openai"
     ],
     python_requires='>=3.7',
 )
