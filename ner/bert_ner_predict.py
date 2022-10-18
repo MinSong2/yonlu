730c40bf173929b6ea399b1a753f3d0e65db4aa5
Min Song <min.song@yonsei.ac.kr>
2022-10-19 06:47:03 +0900

    Update bert_ner_predict.py

--- ner/bert_ner_predict.py
+++ ner/bert_ner_predict.py
@@ -186,16 +186,3 @@ def predict(pred_config):
     logger.info("Prediction Done!")
 
 
-if __name__ == "__main__":
-    init_logger()
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--input_file", default="sample_pred_in.txt", type=str, help="Input file for prediction")
-    parser.add_argument("--output_file", default="sample_pred_out.txt", type=str, help="Output file for prediction")
-    parser.add_argument("--model_dir", default="./model", type=str, help="Path to save, load model")
-
-    parser.add_argument("--batch_size", default=32, type=int, help="Batch size for prediction")
-    parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
-
-    pred_config = parser.parse_args()
-    predict(pred_config)
